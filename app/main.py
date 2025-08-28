8a799c3184989689d66d1ff4b2704a25f3895a0f
Lukasz Kmiecik <kmieciklu@gmail.com>
2025-08-29 00:02:14 +0200

    new solutions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-import pprint
-
 def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
@@ -23,10 +21,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     #     "status": stat
     # }
     return {
-                "errors": [format_linter_error(er) for er in errors],
-                "path": file_path,
-                "status": "passed" if not errors else "failed"
-            }
+        "errors": [format_linter_error(er) for er in errors],
+        "path": file_path,
+        "status": "passed" if not errors else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
@@ -34,167 +32,7 @@ def format_linter_report(linter_report: dict) -> list:
     # for k, v in linter_report.items():
     #     rest.append(format_single_linter_file(k, v))
 
-
-    return [format_single_linter_file(keys, values) for keys, values in linter_report.items()]
-
-
-
-
-error = {
-    "code": "E501",
-    "filename": "./source_code_2.py",
-    "line_number": 18,
-    "column_number": 80,
-    "text": "line too long (99 > 79 characters)",
-    "physical_line": '    return f"I like to filter, rounding, doubling, '
-    "store and decorate numbers: {', '.join(items)}!\"",
-}
-
-errors = [
-    {
-        "code": "E501",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 80,
-        "text": "line too long (99 > 79 characters)",
-        "physical_line": '    return f"I like to filter, rounding, doubling, '
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-    {
-        "code": "E501",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 80,
-        "text": "line too long (99 > 79 characters)",
-        "physical_line": '    return f"I like to filter, rounding, doubling, '
-        "store and decorate numbers: {', '.join(items)}!\"",
-    }
-]
-asdasd = []
-
-report_file = {
-    "./test_source_code_2.py": [],
-    "./source_code_2.py":
-        [
-            {
-                "code": "E501",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 80,
-                "text": "line too long (99 > 79 characters)",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-            {
-                "code": "W292",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 100,
-                "text": "no newline at end of file",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-        ]
-}
-
-
-INPUT_REPORT = {
-    "./test_source_code_2.py": [],
-    "./source_code_2.py": [
-        {
-            "code": "E501",
-            "filename": "./source_code_2.py",
-            "line_number": 18,
-            "column_number": 80,
-            "text": "line too long (99 > 79 characters)",
-            "physical_line": '    return f"I like to filter, rounding, doubling, '
-            "store and decorate numbers: {', '.join(items)}!\"",
-        },
-        {
-            "code": "W292",
-            "filename": "./source_code_2.py",
-            "line_number": 18,
-            "column_number": 100,
-            "text": "no newline at end of file",
-            "physical_line": '    return f"I like to filter, rounding, doubling, '
-            "store and decorate numbers: {', '.join(items)}!\"",
-        },
-    ],
-    "./source_code_1.py": [
-        {
-            "code": "E702",
-            "filename": "./source_code_1.py",
-            "line_number": 3,
-            "column_number": 74,
-            "text": "multiple statements on one line (semicolon)",
-            "physical_line": '        new_items = [f"{key} -> {value}" for key, '
-            "value in items.items()]; return func(new_items)\n",
-        },
-        {
-            "code": "E501",
-            "filename": "./source_code_1.py",
-            "line_number": 3,
-            "column_number": 80,
-            "text": "line too long (97 > 79 characters)",
-            "physical_line": '        new_items = [f"{key} -> {value}" for key, '
-            "value in items.items()]; return func(new_items)\n",
-        },
-        {
-            "code": "E302",
-            "filename": "./source_code_1.py",
-            "line_number": 15,
-            "column_number": 1,
-            "text": "expected 2 blank lines, found 1",
-            "physical_line": "def number_filter(func):\n",
-        },
-        {
-            "code": "E303",
-            "filename": "./source_code_1.py",
-            "line_number": 27,
-            "column_number": 1,
-            "text": "too many blank lines (6)",
-            "physical_line": "@number_filter\n",
-        },
-        {
-            "code": "E501",
-            "filename": "./source_code_1.py",
-            "line_number": 31,
-            "column_number": 80,
-            "text": "line too long (99 > 79 characters)",
-            "physical_line": '    return f"I like to filter, rounding, doubling, '
-            "store and decorate numbers: {', '.join(items)}!\"\n",
-        },
-    ],
-    "./test_source_code_1.py": [
-        {
-            "code": "E302",
-            "filename": "./test_source_code_1.py",
-            "line_number": 4,
-            "column_number": 1,
-            "text": "expected 2 blank lines, found 1",
-            "physical_line": "@pytest.mark.parametrize(\n",
-        },
-        {
-            "code": "E501",
-            "filename": "./test_source_code_1.py",
-            "line_number": 32,
-            "column_number": 80,
-            "text": "line too long (84 > 79 characters)",
-            "physical_line": '            "decorate numbers: 1 -> 2, 2 -> 4, 6 -> 12, -111 -> -222, -50 -> -100!",\n',
-        },
-        {
-            "code": "W292",
-            "filename": "./test_source_code_1.py",
-            "line_number": 112,
-            "column_number": 6,
-            "text": "no newline at end of file",
-            "physical_line": "    )",
-        },
-    ],
-}
-
-
-
-# pprint.pprint(format_linter_error(error), width=100)
-# pprint.pprint(format_single_linter_file("./source_code_2.py", errors), width=100)
-pprint.pprint(format_linter_report(INPUT_REPORT), width = 100)
+    return [
+        format_single_linter_file(keys, values)
+        for keys, values in linter_report.items()
+    ]
