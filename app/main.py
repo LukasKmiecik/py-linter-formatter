dcdea45bceca6d79b982edeb95b24394008d91e6
Lukasz Kmiecik <kmieciklu@gmail.com>
2025-08-28 13:46:27 +0200

    Solution secend funkcjon

--- app/main.py
+++ app/main.py
@@ -1,11 +1,42 @@
+import pprint
+
 def format_linter_error(error: dict) -> dict:
-    print({"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"})
-    return {"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
+
+
+
+
+
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    # bledy = []
+    #
+    # for er in errors:
+    #     bledy.append(format_linter_error(er))
+    #
+    # stat = "passed" if not bledy else "failed"
+    # rest = {
+    #     "errors": bledy,
+    #     "path": file_path,
+    #     "status": stat
+    # }
+    return {
+                "errors": (mors := [format_linter_error(er) for er in errors]),
+                "path": file_path,
+                "status": "passed" if not mors else "failed"
+            }
+
+
+
+
+
 
 
 def format_linter_report(linter_report: dict) -> list:
@@ -27,4 +58,27 @@ error = {
     "store and decorate numbers: {', '.join(items)}!\"",
 }
 
-format_linter_error(error)
+errors = [
+    {
+        "code": "E501",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 80,
+        "text": "line too long (99 > 79 characters)",
+        "physical_line": '    return f"I like to filter, rounding, doubling, '
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+    {
+        "code": "E501",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 80,
+        "text": "line too long (99 > 79 characters)",
+        "physical_line": '    return f"I like to filter, rounding, doubling, '
+        "store and decorate numbers: {', '.join(items)}!\"",
+    }
+]
+asdasd = []
+
+# pprint.pprint(format_linter_error(error), width=100)
+# pprint.pprint(format_single_linter_file("./source_code_2.py", errors), width=100)
